21882c6dc97d5fe2e0d873426c1ea645c944f2ab
Mistudeedesignz <Oladayoawe12@gmail.com>
2022-06-28 17:46:12 -0700

    commit

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+for int in range(0, 100):
+    if int != 99:
+        print("{:02d},".format(int), end=" ")
+    else:
+        print("{:02}".format(int))
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"5-print_comb2.py" [New File]                                 0,0-1         All
+
