3059f64bae606027d5dd7df14ff89ca76c96b928
Mistudeedesignz <Oladayoawe12@gmail.com>
2022-06-30 17:58:57 -0700

    commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
 
     from add_0 import add
 
-    b = 1
-    d = 2
+    a = 1
+    b = 2
 
-    print("{:d} + {:d} = {:d}".format(b, d, add(b, d)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
