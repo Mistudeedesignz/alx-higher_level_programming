69fa2ea85eabf4edcab85ab83481ef6e86394d3e
Mistudeedesignz <Oladayoawe12@gmail.com>
2022-09-07 12:26:50 -0700

    commit

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,27 @@
+-- lists all databases in MySQL server
+-- show databases in the server
+SHOW DATABASES;
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"0-list_databases.sql" [New File]                             0,0-1         All
+
